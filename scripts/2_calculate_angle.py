d1afeef7a29dcee31b9e7c4394200a787b597ef9
choeyujeong911 <yujeongchoe20@gmail.com>
2025-02-13 15:19:44 +0800

    Complete

--- scripts/2_calculate_angle.py
+++ scripts/2_calculate_angle.py
@@ -1,105 +1,63 @@
 #! /usr/bin/env python3
 
 import numpy as np
-from math import pi, atan, acos
+import math
 import rospy
 from zed_interfaces.msg import Object
-from visualization_msgs.msg import Marker
-from geometry_msgs.msg import Point
 from sensor_msgs.msg import JointState
-from std_msgs.msg import ColorRGBA
 
 
-DE2RA = pi / 180.00
-RA2DE = 180.00 / pi
 X = 0
 Y = 1
 Z = 2
 
 
 def rotate_pitch_negative_90(coord: list) -> list:
+	"""3차원 좌표를 y축을 기준으로(pitch) -90도만큼 변환하는 함수
+
+	Fields
+	----------
+	rotation_matrix
+		pitch -90만큼 회전시키는 행렬
+
+	"""
 	rotation_matrix = np.array([[0, 0, 2], [0, 1, 0], [-1, 0, 0]])
 	orig = np.array(coord)
-	rotated = rotation_matrix @ original
+	rotated = rotation_matrix @ orig
+
+	rospy.loginfo(f'>> Target position : {coord} -> {rotated.tolist()}')
 	return rotated.tolist()
 
 
-def calculate_xy_angle(coord1: float, coord2: float) -> float:
-	if coord1 >= -0.05 and coord1 <= 0.05:
-		return 0.0
-	else:
-		slope = coord2 / coord1
-		if slope >= 0:
-			return -1.57 + atan(slope)
-		else:
-			return 1.57 + atan(slope)
-			
-
-def calculate_yz_angle(y: float, z: float) -> float:
-	m = float(y / (((y ** 2) + (z ** 2)) ** 0.5))
-	if acos(m) > 0.0: angle = -1.57 + acos(m)
-	else: angle = 1.57 + acos(m)
-	
-	if angle < 0.0:
-		angle = -1.56
-	else:
-		angle = 1.56
-	return angle
-			
-
-def draw_line(p: Point) -> Marker:
-	line = Marker()
-	line.header.frame_id = "base_link"
-	line.header.stamp = rospy.Time.now()
-	line.ns = 'guide_line'
-	line.id = 0
-	line.type = Marker.ARROW
-	line.action = Marker.ADD
-	line.scale.x = 0.02
-	line.scale.y = 0.05
-	line.scale.z = 0.1
-	line.color = ColorRGBA(0.0, 1.0, 0.0, 0.5)	
-	
-	start_point = Point()
-	start_point.x = 0.0
-	start_point.y = 0.0
-	start_point.z = 0.107
-	
-	end_point = Point()
-	end_point.x = p.x
-	end_point.y = p.y
-	end_point.z = p.z
-	
-	line.points.append(start_point)
-	line.points.append(end_point)
-	
-	return line
+def calculate_slope_to_degree(a: float, b: float) -> float:
+	"""
+	원점 (0,0)과 점 (a,b)를 잇는 직선의 기울기를 degree(도) 단위로 계산하는 함수
+	"""
+	angle_rad = math.atan2(b, a)
+	angle_deg = math.degrees(angle_rad)
+	return angle_deg
 
 
-def target_callback(msg: Object):
+def target_callback(msg: Object) -> None:
+	"""
+	타겟 객체의 위치 정보를 토대로 각 서보모터의 회전 각도를 계산하고 발행하는 함수
+	"""
+	rotated_coord = rotate_pitch_negative_90(msg.position)
 	rate = rospy.Rate(10)
-	guide_pub = rospy.Publisher('guide_line', Marker, queue_size=1)
-	
 	angle_pub = rospy.Publisher('joint_states', JointState, queue_size=1)
+
 	angle = JointState()
 	angle.header.frame_id = ''
 	angle.header.stamp = rospy.Time.now()
-	angle.name = ['joint1', 'joint2']
-	
-	rotated_coord = rotate_pitch_negative_90(msg.position)
-	
-	joint1 = calculate_xy_angle(msg.position[X], msg.position[Y])
-	joint2 = calculate_yz_angle(msg.objects[0].position[Y], (msg.objects[0].position[Z]) - 0.11)
-	
-	angle.position = [joint1, joint2]
 	angle.velocity = []
 	angle.effort = []
 	
-	pnt = Point()
-	pnt.x = msg.objects[0].position[X]
-	pnt.y = msg.objects[0].position[Y]
-	pnt.z = msg.objects[0].position[Z]
-	guide_pub.publish(draw_line(pnt))	#msg.points[0]
+	joint1 = calculate_slope_to_degree(rotated_coord[X], rotated_coord[Y])
+	joint2 = calculate_slope_to_degree(rotated_coord[Y], rotated_coord[Z])
+	angle.position = [joint1, joint2]
+	angle.name = ['joint1', 'joint2']
+	rospy.loginfo(f'>> Joint angle : {joint1:.2f}, {joint2:.2f}')
+
 	angle_pub.publish(angle)
 	rate.sleep()
 	
