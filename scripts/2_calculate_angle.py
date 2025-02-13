a4329559b35e993e9b6e14ba4d4bae1fbfcb4150
choeyujeong911 <yujeongchoe20@gmail.com>
2025-02-13 13:04:20 +0800

    .

--- scripts/2_calculate_angle.py
+++ scripts/2_calculate_angle.py
@@ -3,7 +3,7 @@
 import numpy as np
 from math import pi, atan, acos
 import rospy
-from zed_interfaces.msg import ObjectsStamped
+from zed_interfaces.msg import Object
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
 from sensor_msgs.msg import JointState
@@ -11,11 +11,17 @@ from std_msgs.msg import ColorRGBA
 
 
 DE2RA = pi / 180.00
-RA2DE = 180 / pi
+RA2DE = 180.00 / pi
 X = 0
 Y = 1
 Z = 2
-PITCH_MINUS_90 = np.array([[0, 0, 2], [0, 1, 0], [-1, 0, 0]])
+
+
+def rotate_pitch_negative_90(coord: list) -> list:
+	rotation_matrix = np.array([[0, 0, 2], [0, 1, 0], [-1, 0, 0]])
+	orig = np.array(coord)
+	rotated = rotation_matrix @ original
+	return rotated.tolist()
 
 def calculate_xy_angle(coord1: float, coord2: float) -> float:
 	if coord1 >= -0.05 and coord1 <= 0.05:
@@ -66,26 +72,20 @@ def draw_line(p: Point) -> Marker:
 	
 	return line
 
-def object_callback(msg):
-
-	#print(f'> Original z : {msg.points[0].z:.4f} -> {(msg.points[0].z-0.11):.4f}')
-	print(f'> Original z : {msg.objects[0].position[Z]:.4f} -> {(msg.objects[0].position[Z]-0.11):.4f}')
+def object_callback(msg: Object):
 	rate = rospy.Rate(15)
 	guide_pub = rospy.Publisher('guide_line', Marker, queue_size=1)
 	
 	angle_pub = rospy.Publisher('joint_states', JointState, queue_size=1)
 	angle = JointState()
-	#angle.header.frame_id = ''
-	angle.header.frame_id = msg.header.frame_id
+	angle.header.frame_id = ''
 	angle.header.stamp = rospy.Time.now()
 	angle.name = ['joint1', 'joint2']
 	
-	joint1 = calculate_xy_angle(msg.objects[0].position[X], msg.objects[0].position[Y])		# msg.points[0].x, msg.points[0].y
-	joint2 = calculate_yz_angle(msg.objects[0].position[Y], (msg.objects[0].position[Z]) - 0.11)	# msg.points[0].y, (msg.points[0].z - 0.11)
-	#joint2 = calculate_angle(msg.points[0].y, (msg.points[0].z - 0.11))
-	#joint2 = -1 * atan(msg.points[0].y / (msg.points[0].z - 0.11))
+	rotated_coord = rotate_pitch_negative_90(msg.position)
 	
-	print(f'> Servo 1 : {joint1:.4f} , Servo 2 : {joint2:.4f}')
+	joint1 = calculate_xy_angle(msg.position[X], msg.position[Y])
+	joint2 = calculate_yz_angle(msg.objects[0].position[Y], (msg.objects[0].position[Z]) - 0.11)
 	
 	angle.position = [joint1, joint2]
 	angle.velocity = []
@@ -101,6 +101,5 @@ def object_callback(msg):
 	
 if __name__ == '__main__':
 	rospy.init_node('calculate_servo_angle')
-	#subscriber = rospy.Subscriber('/tracking_point', Marker, object_callback)
-	subscriber = rospy.Subscriber('/zed2i/zed_node/obj_det/objects', ObjectsStamped, object_callback) 
+	subscriber = rospy.Subscriber('/headlamp/target_object', Object, object_callback) 
 	rospy.spin()
