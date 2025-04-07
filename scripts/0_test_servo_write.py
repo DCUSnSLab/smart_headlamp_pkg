45bc6e51503ee10530b265bf98662a8235af61f5
choeyujeong911 <yujeongchoe20@gmail.com>
2025-04-07 15:53:54 +0800

    Add topic for visualizing in Rviz

--- scripts/0_test_servo_write.py
+++ scripts/0_test_servo_write.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 import math
+import rospy
 import Arm_Lib
 import time
+from sensor_msgs.msg import JointState
 
 if __name__ == '__main__':
+	rospy.init_node('servo_test')
 	sbus = Arm_Lib.Arm_Device()
 	angle = 90
 	servo = -1
@@ -12,7 +15,22 @@ if __name__ == '__main__':
 		servo = int(input(">> servo : "))
 		if servo == 0: break
 		angle_r = float(input(">> angle(RA) : "))
-		angle_d = math.degrees(angle_r)
+		angle_d = math.degrees(angle_r) + 90
 		print(f'Degree == {angle_d:.4f}')
 		sbus.Arm_serial_servo_write(servo, angle_d, 100)
 		print()
+
+		rate = rospy.Rate(10)
+		ang = JointState()
+		ang_pub = rospy.Publisher('joint_states', JointState, queue_size=1)
+
+		ang.header.frame_id = ''
+		ang.header.stamp = rospy.Time.now()
+		ang.velocity = []
+		ang.effort = []
+		ang.position = [angle_r]
+		jnt_name = "joint" + str(servo)
+		ang.name = [jnt_name]
+
+		ang_pub.publish(ang)
+		rate.sleep()
