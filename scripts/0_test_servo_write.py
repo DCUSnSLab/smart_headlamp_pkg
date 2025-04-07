9bae61befb3b2e1ccb21386e6c8cc810caff3f8f
choeyujeong911 <yujeongchoe20@gmail.com>
2025-04-07 13:10:02 +0800

    inish test of servo3

--- scripts/0_test_servo_write.py
+++ scripts/0_test_servo_write.py
@@ -4,7 +4,6 @@ import math
 import Arm_Lib
 import time
 
-
 if __name__ == '__main__':
 	sbus = Arm_Lib.Arm_Device()
 	angle = 90
@@ -17,10 +16,3 @@ if __name__ == '__main__':
 		print(f'Degree == {angle_d:.4f}')
 		sbus.Arm_serial_servo_write(servo, angle_d, 100)
 		print()
-
-
-	#while ans != '0':
-		# ang2 = int(input("2번 서보 몇 도 회전 ? >> "))
-		# sbus.Arm_serial_servo_write(2, ang2, 100)
-
-		# ans = input(">> 계속하려면 아무 키나 누르세요... (종료: 0)  ")
