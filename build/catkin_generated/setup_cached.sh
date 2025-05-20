e8fde9a95391d018099f1aff8a8d1d5d73d8fdff
choeyujeong911 <yujeongchoe20@gmail.com>
2025-05-20 11:56:47 +0800

    Why did arduino add build/ in pkg?

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/dofbot/dofbot_ws/src/smart_headlamp_pkg/build/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/dofbot/.vscode-server/cli/servers/Stable-848b80aeb52026648a8ff9f7c45a9b0a80641e2e/server/bin/remote-cli:/home/dofbot/.local/bin:/home/dofbot/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/dofbot/bin'
+export PWD='/home/dofbot/dofbot_ws/src/smart_headlamp_pkg/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/dofbot/dofbot_ws/src/smart_headlamp_pkg/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/dofbot/dofbot_ws/src/smart_headlamp_pkg:$ROS_PACKAGE_PATH"
